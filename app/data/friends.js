38f46c99f36989ed2e84f858331760838e250bef
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-24 20:42:47 -0700

    Finalized HTML and added friends[i].scores values into friends.js friends array of objects for each friends[i].name character

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friends = [ {
+	"name" : "Bad Luck Brian",
+	"photo" : "https://sites.google.com/a/umich.edu/the-rhetoric-of-memes/_/rsrc/1386351632076/home/bad-luck-brian/Firetruck-Bad-Luck-Brian.jpg",
+	"scores" : [1,1,3,5,2,3,4,1,4,2]
+}, {
+	"name" : "Forever Alone Troll",
+	"photo" : "http://www.laughaton.com/uploads/images/941-Forever-alone.jpg",
+	"scores" : [3,2,4,1,2,5,2,1,3,1]
+}, {
+	"name" : "F.R.I.E.N.D.S",
+	"photo" : "http://i1.ytimg.com/vi/_iBpTqp8nX8/hqdefault.jpg",
+	"scores" : [5,2,3,1,4,2,3,1,2,5]
+}];
+
+
+module.exports = friends;
