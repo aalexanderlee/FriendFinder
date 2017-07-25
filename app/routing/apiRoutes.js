22e82a3ddd3a6e8bb446889ac93cf8885dcc54a2
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-24 21:00:00 -0700

    Adding routes to apiRoutes.js using data from friends.js using express() package

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,14 @@
 // Includes two routes:
-// GET route to "/api/friends" in displaytable.html 
+// GET route to "/api/friends" to get from friends.js 
+
+// Grab the saved data from friends array of friend profile objects
+var friendArr = require('../data/friends.js');
+
+//Use express() to access api of friends saved
+module.exports = function (app) {
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendArr);
+	})
+
+
