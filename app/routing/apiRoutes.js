331b3679afd00c1ee03cb4d3c6b2b2f2c922164a
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-24 21:52:23 -0700

    Pushing differences between the user choice values and saved friend api scores into an array for comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,4 +24,21 @@ module.exports = function (app) {
 				user.scores[i] = parseInt(user.scores[i]); //change 2,3, or 4 to an int
 			}
 		}
-		
+
+		var diffArr = []; //this will be the new .score for the user object in api
+
+		for(var i = 0; i < friendArr.length; i++) {
+			//var comparedFriend = friendArr[i];
+			var diffTotal = 0;
+			
+			for(var j = 0; j < friendArr[i].scores.length; j++) {
+				var diffScore = Math.abs(friendArr[i].scores[k] - user.scores[j]);
+				diffTotal += diffScore;
+			}
+			diffArr[i] = diffTotal;
+		}
+
+
+
+
+
