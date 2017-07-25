02fb19744ee029127ec854545fbe2c757da10b59
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-24 20:49:58 -0700

    Adding paths to the HTML pages using the app.get and app.use functions and syntax

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,15 @@
 // Includes two routes:
 // GET route to "/survey" path to survey.html
 // USE route to "/" path to home.html
+// Use the built in "path" 
+var path = require('path');
+
+module.exports = function(app) {
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+
+	app.use('/', function(req, res){
+		res.sendFile(path.join(__dirname + '../public/home.html'));
+	});
+}
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-// Dependencies; Packages; Connections
-// =============================================================
+// Sets up Dependencies and Packages
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// Sets up the Express App and localhost 3000
 var app = express();
 var PORT = 3000;
